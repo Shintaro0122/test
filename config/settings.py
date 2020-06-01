58786830ee09f591e6525ecd75ca17b966d3c0e4
IMOKURI <nenegi.01mo@gmail.com>
2020-06-01 22:32:24 +0900

    update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's@3y9m*!h%ev_%dtjv2&f-y3%$$lnqov!d7jw17xf-k(cz4+95'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
